e0bfd6c5d148a63834d91da2a2e2c704975259ec
terryoy <terry.ouyang@gmail.com>
2015-06-15 13:53:30 +0800

    add upload folder settings

--- app/app/local_settings.py
+++ app/app/local_settings.py
@@ -3,4 +3,5 @@ from .settings import *
 # change to the place where you put your static files (e.g. where "/srv/www/django-skeleton/" is web root)
 STATIC_ROOT = "/srv/www/django-skeleton/static"
 
-
+# an upload folder where you upload files to (can use symbolic link to set under app folder)
+MEDIA_ROOT = os.path.join(BASE_DIR, "upload")
